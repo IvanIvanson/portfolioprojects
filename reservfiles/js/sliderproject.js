f11bcab64f779d827754a49d2ffa185b9c196ea8
IvanIvanson <ghalut1977@gmail.com>
2023-05-04 21:47:04 +0300

    commit10

---
+++ reservfiles/js/sliderproject.js
@@ -0,0 +1,37 @@
+const swiper2 = new Swiper(".swiper2", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".slider__arrow-right2",
+    prevEl: ".slider__arrow-left2",
+  },
+});
+
+
+// window.onload = function () {
+  
+  let projectContainer = document.querySelector(".project-slider");
+  let images = document.querySelectorAll(".project-slider img");
+  
+  
+  let swiperCont2 = document.querySelector(".swiper2");
+
+  if (swiperCont2.clientHeight < 50) {
+    swiperCont2.style.display = "none";
+    projectContainer.classList.add("project-slider-bg");
+    images.forEach(item => {
+      item.classList.remove('img-project');
+    })
+  }
+  else {
+    swiperCont2.style.display = "block";
+    projectContainer.classList.remove("project-slider-bg");
+     images.forEach((item) => {
+       item.classList.add("img-project");
+     });
+  }
+
+// }
---
+++ reservfiles/js/slidersertyficate.js
@@ -0,0 +1,28 @@
+const swiper1 = new Swiper(".swiper1", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+ 
+  navigation: {
+    nextEl: ".slider__arrow-right",
+    prevEl: ".slider__arrow-left",
+  },
+});
+
+
+  let sertyficatesContainer = document.querySelector(".certificates-slider");
+ 
+
+  let swiperCont1 = document.querySelector(".swiper1");
+  
+  
+  if (swiperCont1.clientHeight < 50) {
+   swiperCont1.style.display = "none";
+   sertyficatesContainer.classList.add("certificatas-bg");
+    
+  }
+  else {
+     swiperCont1.style.display = "block";
+    sertyficatesContainer.classList.remove("certificatas-bg");
+  }
